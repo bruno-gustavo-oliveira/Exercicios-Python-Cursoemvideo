de592823b64491679bce33a2f720fa58b53f0e45
bruno-gustavo-oliveira <bruno555883528@gmail.com>
2026-06-18 23:09:47 -0300

    Exercício 3

---
+++ Exercicios/ex003.py
@@ -0,0 +1,4 @@
+#Crie um programa que leia dois números e mostre a soma entre eles.
+n1 = int(input('Informe o um número: '))
+n2 = int(input('Informe outro número: '))
+print('A soma de {} e {} é: {}'.format(n1,n2,(n1+n2)))
